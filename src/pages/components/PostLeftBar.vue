a28829086a4acbcd6c27ff7217b3cf7a006fbb2e
BangGahee <gahee.bang@cj.net>
2023-11-27 17:22:28 +0900

    커뮤니티 글쓰기

--- src/pages/components/PostLeftBar.vue
+++ src/pages/components/PostLeftBar.vue
@@ -1,19 +1,20 @@
 <template>
-  <aside>
-    <q-cardv flat bordered>
+  <StickySideBar>
+    <q-card flat bordered>
       <q-list bordered separator>
-        <q-item clickable v-ripple>
+        <q-item clickable v-ripple :active="true">
           <q-item-section>전체</q-item-section>
         </q-item>
         <q-item v-for="cate in categories" :key="cate.value" clickable v-ripple>
           <q-item-section>{{ cate.label }}</q-item-section>
         </q-item>
       </q-list>
-    </q-cardv>
-  </aside>
+    </q-card>
+  </StickySideBar>
 </template>
 
 <script setup>
+import StickySideBar from 'src/components/StickySideBar.vue';
 const categories = [
   { label: 'Q&A', value: 'qna' },
   { label: '커뮤니티', value: 'community' },
@@ -21,5 +22,3 @@ const categories = [
   { label: '강의', value: 'lecture' },
 ];
 </script>
-
-<style lang="scss" scoped></style>
